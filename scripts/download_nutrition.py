9638949eda88471424a750c7cc31bd39bec937c8
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-04-09 11:13:12 -0500

    adds multiple ingredientrequests

--- scripts/download_nutrition.py
+++ scripts/download_nutrition.py
@@ -7,6 +7,18 @@ ingredients = ["blueberries", "strawberries", "almond milk"]
 def get_nutrition_data(ingredient):
     response = requests.get(f'https://api.edamam.com/api/nutrition-data?app_id=22eb6145&app_key=%20b4e0e731b77176135ba3b820c7bf1ac8&nutrition-type=cooking&ingr={ingredient}')
     data = response.json()
-    print(data)
+    return data
 
-get_nutrition_data(ingredients[1])
+
+def make_json():
+    all_data = []
+    
+    for ingredient in ingredients:
+        api_data = get_nutrition_data(ingredient)
+        all_data.append(api_data)
+
+    with open('nutrition_data.json', 'w') as nutrition_file:
+        json.dump(all_data, nutrition_file, indent=4)
+        
+
+make_json()
