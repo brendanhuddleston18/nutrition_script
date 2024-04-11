8bd72969abb23e039b7ed6a42bc5fbe9f4e8fd80
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-04-10 22:45:59 -0500

    adds nutritional analysis link for potential use in GET request

--- scripts/download_nutrition.py
+++ scripts/download_nutrition.py
@@ -3,7 +3,8 @@ import json
 
 ingredients = ["blueberries", "strawberries", "almond milk"]
 
-
+# Check this out
+# https://api.edamam.com/doc/open-api/nutrition-analysis-v1.json
 def get_nutrition_data(ingredient):
     response = requests.get(f'https://api.edamam.com/api/nutrition-data?app_id=22eb6145&app_key=%20b4e0e731b77176135ba3b820c7bf1ac8&nutrition-type=cooking&ingr={ingredient}')
     data = response.json()
